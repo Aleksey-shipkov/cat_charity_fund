db629b902ce21f672bbe98ba4e948b7210203f7c
Anya S <shipkov.alexey@gmail.com>
2023-04-09 13:53:43 +0300

    Donev4

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -1,5 +1,4 @@
 from app.api.endpoints.user import router as user_router  # noqa
-from app.api.endpoints.charityproject import (
-    router as charity_project_router,
-)  # noqa
+from app.api.endpoints.charityproject import (  # noqa 
+    router as charity_project_router)  # noqa
 from app.api.endpoints.donation import router as donation_router  # noqa
